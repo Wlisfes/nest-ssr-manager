b292feb890e7333617d7836812b0aac0df37d1a8
妖雨纯 <876451336@qq.com>
2025-07-07 15:41:15 +0800

    ✨ feat(apps):新增web-remote-ssr-server服务

---
+++ apps/web-remote-ssr-server/src/app.controller.ts
@@ -0,0 +1,15 @@
+import { Controller, Get, Header, Request } from '@nestjs/common'
+import { createRouteServer } from '@web-remote-ssr-server/vite.server'
+
+@Controller()
+export class AppController {
+    @Get('*')
+    @Header('Content-Type', 'text/html')
+    async fetchBaseRender(@Request() request) {
+        try {
+            return await createRouteServer(request)
+        } catch (error) {
+            return '500'
+        }
+    }
+}
---
+++ apps/web-remote-ssr-server/src/main.ts
@@ -0,0 +1,23 @@
+import { NestExpressApplication } from '@nestjs/platform-express'
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from '@web-remote-ssr-server/app.module'
+import { createViteServer } from '@web-remote-ssr-server/vite.server'
+import { resolve } from 'path'
+import compression from 'compression'
+
+async function bootstrap() {
+    const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+        cors: true
+    })
+    if (process.env.NODE_ENV === 'production') {
+        app.useStaticAssets(resolve(process.cwd(), 'build/web-remote/client'), { index: false })
+        app.use(compression())
+    } else {
+        const vite = await createViteServer()
+        await app.use(vite.middlewares)
+    }
+    return app.listen(process.env.NODE_REMOTE_SSR_PORT).then(() => {
+        console.log(`客户端SSR服务启动[${process.env.NODE_ENV}]:`, `http://localhost:${process.env.NODE_REMOTE_SSR_PORT}`)
+    })
+}
+bootstrap()
