4cdf1e8f67402751f040dc4c168f6b71ebdb0498
妖雨纯 <876451336@qq.com>
2025-07-10 17:21:58 +0800

    ✨ feat(server/guard): 修改管理端守卫

--- server/guard/index.ts
+++ server/guard/index.ts
@@ -1,3 +1,3 @@
 export * from '@server/guard/modules/auth.throttler.guard'
-export * from '@server/guard/modules/auth.system.guard'
+export * from '@server/guard/modules/auth.remote.guard'
 export * from '@server/guard/modules/auth.client.guard'
--- server/guard/modules/auth.remote.guard.ts
+++ server/guard/modules/auth.remote.guard.ts
@@ -1,20 +1,20 @@
 import { CanActivate, SetMetadata, ExecutionContext, Injectable, HttpException, HttpStatus } from '@nestjs/common'
 import { Reflector } from '@nestjs/core'
 
-export interface AuthSystemOptions {
+export interface AuthRemoteOptions {
     /**开启token验证**/
     check?: boolean
 }
 
 @Injectable()
-export class AuthSystemGuard implements CanActivate {
+export class AuthRemoteGuard implements CanActivate {
     /**登录拦截入口**/
     public async canActivate(context: ExecutionContext): Promise<boolean> {
         return true
     }
 }
 
-/**登录守卫、使用ApiSystemGuardReflector守卫的接口会验证登录**/
-export const ApiSystemGuardReflector = (options: AuthSystemOptions) => {
-    return SetMetadata(`APP_AUTH_SYSTEM_CONTEXT`, options)
+/**登录守卫、使用ApiRemoteGuardReflector守卫的接口会验证登录**/
+export const ApiRemoteGuardReflector = (options: AuthRemoteOptions) => {
+    return SetMetadata(`APP_AUTH_REMOTE_CONTEXT`, options)
 }
